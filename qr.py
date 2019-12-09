fee7eb9f3566448523702139769b9fa1f1d0c214
hadpeter <hadpeter@umich.edu>
2019-12-09 10:56:14 -0500

    now we have a line but a bad line

--- qr.py
+++ qr.py
@@ -4,37 +4,18 @@ from numpy.linalg import inv
 from numpy.linalg import qr
 from matplotlib import pyplot
 import pandas as pd
-data = pd.read_csv('waterData6.csv')
+data = pd.read_csv('waterData7.csv')
 X = data['T_degC'].values
 Y = data['Salnty'].values
 
 #X, y = data[:,0], data[:,1]
 X = X.reshape((len(X), 1))
-# QR decomposition
-print(X)
-print("That was x")
 X = array(X)
-print(X)
-print("Updated X above")
+# QR decomposition
 Q, R = qr(X)
-#print(Q)
-print("and now b")
-#print(R)
-b = inv(R).dot(Q.T).dot(Y)
 At = inv(R).dot(Q.T)
-print(At)
-print("that was At")
-AtSmall = At/1000
-print(AtSmall)
-print("that was small")
-#print("this is Y below")
-#print(Y)
-AtSmallb = AtSmall.dot(Y)
-print("and AtSmallb")
-print(AtSmallb)
-#print("and b")
-#print(b)
-# predict using coefficients
+b = At.dot(Y)
+print(b)
 yhat = X.dot(b)
 # plot data and predictions
 pyplot.scatter(X, Y)
